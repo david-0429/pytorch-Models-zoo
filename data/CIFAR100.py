bb9dbbfa9094822cdfbe7a9b436c73cd60bc254a
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 23:00:52 +0900

    Update CIFAR100.py

--- data/CIFAR100.py
+++ data/CIFAR100.py
@@ -17,7 +17,7 @@ data_path = '/content/data/CIFAR100'
 
 def CIFAR100_train_loader(args):
   global transform_train
-  transform_train = transform_train(CIFAR100_TRAIN_MEAN , CIFAR100_TRAIN_STD, args = args.DA)
+  transform_train = transform_train(CIFAR100_TRAIN_MEAN , CIFAR100_TRAIN_STD)
 
   trainset = torchvision.datasets.CIFAR100(root=data_path, train=True, download=True, transform=transform_train)
   train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4)
@@ -25,7 +25,7 @@ def CIFAR100_train_loader(args):
 
 def CIFAR100_test_loader(args):
   global transform_test
-  transform_test = transform_test(CIFAR100_TRAIN_MEAN, CIFAR100_TRAIN_STD, args = args.DA_test)
+  transform_test = transform_test(CIFAR100_TRAIN_MEAN, CIFAR100_TRAIN_STD)
 
   testset = torchvision.datasets.CIFAR100(root=data_path, train=False, download=True, transform=transform_test)
   test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
