f737ff75f0c0d43c8d12d6c4c72f5a7dc2ecbeec
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 14:24:30 +0900

    Create CIFAR100.py

---
+++ data/CIFAR100.py
@@ -0,0 +1,25 @@
+import os
+import time
+import numpy as np
+import argparse
+
+import torch
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+
+import utils
+
+data_path = '/content/data/CIFAR100'
+
+def train_loader():
+  trainset = torchvision.datasets.CIFAR100(root=data_path, train=True, download=True, transform=transform_train)
+  train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+  
+  return train_loader
+
+def test_loader():
+  testset = torchvision.datasets.CIFAR100(root=data_path, train=False, download=True, transform=transform_test)
+  test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+  
+  return test_loader
