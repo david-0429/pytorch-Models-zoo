f130f9fab0cdfb0aa1fae28cc4aa64c2cae867d5
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 14:25:10 +0900

    Update CIFAR10.py

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -8,17 +8,18 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 
+import utils
 
-
+data_path = '/content/data/CIFAR10'
 
 def train_loader():
-  trainset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform_train)
+  trainset = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform_train)
   train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4)
   
   return train_loader
 
 def test_loader():
-  testset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform_test)
+  testset = torchvision.datasets.CIFAR10(root=data_path, train=False, download=True, transform=transform_test)
   test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
   
   return test_loader
