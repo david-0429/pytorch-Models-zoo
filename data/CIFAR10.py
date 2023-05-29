ef8af6f4143a848149785f1177e1aad76e3bab85
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 14:18:31 +0900

    Create CIFAR10.py

---
+++ data/CIFAR10.py
@@ -0,0 +1,24 @@
+import os
+import time
+import numpy as np
+import argparse
+
+import torch
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+
+
+
+
+def train_loader():
+  trainset = torchvision.datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform_train)
+  train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+  
+  return train_loader
+
+def test_loader():
+  testset = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform_test)
+  test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+  
+  return test_loader
