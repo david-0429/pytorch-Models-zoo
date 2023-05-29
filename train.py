ee0c7b45224a6dc32fdf137576e9385e295ff263
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 21:33:07 +0900

    Update train.py

--- train.py
+++ train.py
@@ -17,19 +17,24 @@ from data.CIFAR100 import CIFAR100_train_loader, CIFAR100_test_loader
 
 from utils import get_network
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-data', default='CIFAR100', type=str, choices=['CIFAR10', 'CIFAR100'])
-parser.add_argument('-name', type=str)
-parser.add_argument('-net', type=str, required=True, help='net type')
-parser.add_argument('-epochs', default=200, type=int, help='number of total epochs to run')
-parser.add_argument('-batch_size', default=128, type=int, help='mini-batch size (default: 256)')
-parser.add_argument('-lr', default=0.01, type=float, help='initial learning rate')
-#parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
-parser.add_argument('-DA', default='flip_crop', type=str, choices=['non', 'flip_crop', 'flip_crop_AA', 'flip_crop_RA'])
-parser.add_argument('-DA_test', default='non', type=str)
-parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+def parse():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('data', required=True, default='CIFAR100', type=str, choices=['CIFAR10', 'CIFAR100'])
+  parser.add_argument('name', type=str)
+  parser.add_argument('net', type=str, required=True, default='resnet50', help='net type')
+  parser.add_argument('epochs', default=200, type=int, help='number of total epochs to run')
+  parser.add_argument('batch_size', default=128, type=int, help='mini-batch size (default: 256)')
+  parser.add_argument('lr', default=0.01, type=float, help='initial learning rate')
+  #parser.add_argument('warm', type=int, default=1, help='warm up training phase')
+  parser.add_argument('DA', default='flip_crop', type=str, choices=['non', 'flip_crop', 'flip_crop_AA', 'flip_crop_RA'])
+  parser.add_argument('DA_test', default='non', type=str)
+  parser.add_argument('gpu', action='store_true', type=bool, default=False, help='use gpu or not')
 
-args = parser.parse_args()
+  args = parser.parse_args()
+
+  return args
+
+args = parse()
 net = get_network(args)
 
 
@@ -76,9 +81,8 @@ def train(net, epoch):
     
     for batch_index, (images, labels) in enumerate(train_loader):
 
-        if args.gpu:
-            labels = labels.cuda()
-            images = images.cuda()
+        labels = labels.cuda()
+        images = images.cuda()
 
         optimizer.zero_grad()
         outputs = net(images)
@@ -116,8 +120,7 @@ def test(net):
     
     for batch_idx, (images, targets) in enumerate(test_loader):
         
-        if args.gpu:
-            images, targets = images.cuda(), targets.cuda()
+        images, targets = images.cuda(), targets.cuda()
             
         outputs = net(images)
         loss = loss_function(outputs, targets)
