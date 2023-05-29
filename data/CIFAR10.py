66c5821477c77b3ae1e1e9be8b0ae5963aca830a
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-29 22:40:44 +0900

    Update CIFAR10.py

--- data/CIFAR10.py
+++ data/CIFAR10.py
@@ -15,31 +15,17 @@ from conf import CIFAR10_TRAIN_MEAN, CIFAR10_TRAIN_STD
 data_path = '/content/data/CIFAR10'
 
 
-def transform_train():
-  from train import parse
-  args = parse()
-
+def CIFAR10_train_loader(args):
+  global transform_train
   transform_train = transform_train(CIFAR10_TRAIN_MEAN , CIFAR10_TRAIN_STD, args = args.DA)
-  return transform_train
-
-def transform_test():
-  from train import parse
-  args = parse()
-
-  transform_test = transform_test(CIFAR10_TRAIN_MEAN, CIFAR10_TRAIN_STD, args = args.DA_test)
-  return transform_test
-
-def CIFAR10_train_loader():
-  from train import parse
-  args = parse()
 
   trainset = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform_train)
   train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4)
   return train_loader
 
-def CIFAR10_test_loader():
-  from train import parse
-  args = parse()
+def CIFAR10_test_loader(args):
+  global transform_test
+  transform_test = transform_test(CIFAR10_TRAIN_MEAN, CIFAR10_TRAIN_STD, args = args.DA_test)
 
   testset = torchvision.datasets.CIFAR10(root=data_path, train=False, download=True, transform=transform_test)
   test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
