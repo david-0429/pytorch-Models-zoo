5aebaa884fffbffb85ff8aa0dabe4c6547b26ea4
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-05-30 13:31:14 +0900

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def parse_option():
   parser.add_argument('--net', type=str, default='resnet50', help='net type')
   parser.add_argument('--epochs', default=200, type=int, help='number of total epochs to run')
   parser.add_argument('--batch_size', default=128, type=int, help='mini-batch size (default: 256)')
-  parser.add_argument('--lr', default=0.01, type=float, help='initial learning rate')
+  parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate')
   #parser.add_argument('--warm', type=int, default=1, help='warm up training phase')
   parser.add_argument('--DA', default='flip_crop', type=str, choices=['non', 'flip_crop', 'flip_crop_AA', 'flip_crop_RA'])
   parser.add_argument('--DA_test', default='non', type=str)
