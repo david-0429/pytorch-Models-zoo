aef14ee2ee78069546425b1631964d965a36bb51
david-0429 <86578317+david-0429@users.noreply.github.com>
2024-05-01 23:24:54 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ def get_network(args, class_num=10, pretrain=False):
     """ return given network
     """
 
-    net = timm.create_model(args.net, pretrained=pretrain, num_classes=class_num)
+    model = timm.create_model(args.model, pretrained=pretrain, num_classes=class_num)
 
     if args.gpu: #use_gpu
-        net = net.cuda()
+        model = model.cuda()
 
-    return net
+    return model
