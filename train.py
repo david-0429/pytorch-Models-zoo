2c1a29388c9f5716cfa8f9e0a28a14de822c75c1
david-0429 <86578317+david-0429@users.noreply.github.com>
2024-05-02 00:15:00 +0900

    Update train.py

--- train.py
+++ train.py
@@ -12,26 +12,28 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
-from data.CIFAR10 import CIFAR10_train_loader, CIFAR10_test_loader
-from data.CIFAR100 import CIFAR100_train_loader, CIFAR100_test_loader
+from data.CIFAR10 import CIFAR10_loader
+from data.CIFAR100 import CIFAR100_loader
 
-from utils import get_network, adjust_learning_rate
+#from model import get_network
 from conf import CIFAR10_CLASS_NUM, CIFAR100_CLASS_NUM
+
 
 def parse_option():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data', default='CIFAR100', type=str, choices=['CIFAR10', 'CIFAR100'])
+  parser.add_argument('--data', default='CIFAR10', type=str, choices=['CIFAR10', 'CIFAR100'])
   parser.add_argument('--name', type=str)
-  parser.add_argument('--net', type=str, default='resnet50', help='net type')
-  parser.add_argument('--pretrain', action='store_true', default=False, help='use pretrained model or not')
+  parser.add_argument('--model', type=str, default='resnet18', help='net type')
+  parser.add_argument('--pretrain', default=False, help='use pretrained model or not')
   
   parser.add_argument('--epochs', default=200, type=int, help='number of total epochs to run')
-  parser.add_argument('--batch_size', default=128, type=int, help='mini-batch size (default: 256)')
+  parser.add_argument('--batch_size', default=64, type=int, help='mini-batch size (default: 256)')
   parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate')
+  '''
   parser.add_argument('--lr_decay', default=False, type=bool, help='learning rate decay')
   parser.add_argument('--lr_decay_epochs', type=str, default='100,150,180', help='where to decay lr, can be a list')
   parser.add_argument('--lr_decay_rate', type=float, default=0.1, help='decay rate for learning rate')
-  
+  '''
   parser.add_argument('--DA', default='flip_crop', type=str, choices=['non', 'flip_crop', 'flip_crop_AA', 'flip_crop_RA'])
   parser.add_argument('--DA_test', default='non', type=str)
   parser.add_argument('--gpu', action='store_true', default=False, help='use gpu or not')
@@ -43,53 +45,44 @@ def parse_option():
 args = parse_option()
 
 
-if args.data =='CIFAR10':
-  if args.pretrain:
-    net = get_network(args, class_num=CIFAR10_CLASS_NUM, pretrain=True)
-  else:
-    net = get_network(args, class_num=CIFAR10_CLASS_NUM, pretrain=False)
-elif args.data =='CIFAR100':
-  if args.pretrain:
-    net = get_network(args, class_num=CIFAR100_CLASS_NUM, pretrain=True)
-  else:
-    net = get_network(args, class_num=CIFAR100_CLASS_NUM, pretrain=False)
-
-
 #wandb init
 print("wandb init")
 def get_timestamp():
     return datetime.now().strftime("%b%d_%H-%M-%S")
-  
+
 wandb.init(
   # Set the project where this run will be logged
-  project=f"{args.data} models zoo", 
-  name=f"{args.net}_{args.DA}_{args.batch_size}_{args.lr}-{get_timestamp()}"
+  project=f"AAAI 2024 coming soon", 
+  name=f"{args.data}_{args.model}_{args.batch_size}_{args.lr}_{get_timestamp()}"
 )
-
+wandb.config.update(args)
 
 #Data_loader
 if args.data == 'CIFAR10':
-  train_loader = CIFAR10_train_loader(args)
-  test_loader = CIFAR10_test_loader(args)
+  train_loader = CIFAR10_loader(args, is_train=True)
+  test_loader = CIFAR10_loader(args, is_train=False)
 elif args.data == 'CIFAR100':
-  train_loader = CIFAR100_train_loader(args)
-  test_loader = CIFAR100_test_loader(args)
-
-
-iterations = args.lr_decay_epochs.split(',')
-args.lr_decay_epochs = list([])
-for it in iterations:
-  args.lr_decay_epochs.append(int(it))
+  train_loader = CIFAR100_loader(args, is_train=True)
+  test_loader = CIFAR100_loader(args, is_train=False)
   
+  
+#model 
+if args.data =='CIFAR10':
+  model = get_network(args, class_num=CIFAR10_CLASS_NUM, pretrain=args.pretrain)
+elif args.data =='CIFAR100':
+  model = get_network(args, class_num=CIFAR100_CLASS_NUM, pretrain=args.pretrain)
+
+
 loss_function = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(model.parameters(), lr=args.lr) # momentum=0.9, weight_decay=5e-4
     
+
 #train    
-def train(net, epoch):
+def train(model, epoch):
 
     epoch_start_time = time.time()
     print('epoch: %d' % epoch)
-    net.train()
+    model.train()
     
     train_loss = 0
     correct = 0
@@ -101,7 +94,7 @@ def train(net, epoch):
         images = images.cuda()
 
         optimizer.zero_grad()
-        outputs = net(images)
+        outputs = model(images)
         loss = loss_function(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -112,27 +105,19 @@ def train(net, epoch):
         correct += predicted.eq(labels.data).cpu().sum().float().item()
         
         b_idx = batch_index
-        
-        '''
-        other tricks : warmup
-        
-        if epoch <= args.warm:
-            warmup_scheduler.step()
-        '''
-
-        wandb.log({"epoch/train_acc": correct / total * 100, "epoch/trn_loss": train_loss / (b_idx + 1), "epoch": epoch})
-
 
     print('Train \t Time Taken: %.2f sec' % (time.time() - epoch_start_time))
     print('Loss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / (b_idx + 1), 100. * correct / total, correct, total))
+
+    wandb.log({"epoch/train_acc": correct / total * 100, "epoch/trn_loss": train_loss / (b_idx + 1), "epoch": epoch})
 
     return train_loss / (b_idx + 1), correct / total * 100
 
 
 #test
-def test(net):
+def test(model):
     epoch_start_time = time.time()
-    net.eval()
+    model.eval()
     test_loss = 0
     correct = 0
     total = 0
@@ -141,7 +126,7 @@ def test(net):
         
         images, targets = images.cuda(), targets.cuda()
             
-        outputs = net(images)
+        outputs = model(images)
         loss = loss_function(outputs, targets)
 
         test_loss += loss.item()
@@ -151,31 +136,20 @@ def test(net):
         
         b_idx = batch_idx
 
-        wandb.log({"epoch/val_acc": correct / total * 100, "epoch/val_loss": test_loss / (b_idx + 1), "epoch": epoch})
-
-
     print('Test \t Time Taken: %.2f sec' % (time.time() - epoch_start_time))
     print('Loss: %.3f | Acc: %.3f%% (%d/%d)' % (test_loss / (b_idx + 1), 100. * correct / total, correct, total))
+    
+    wandb.log({"epoch/val_acc": correct / total * 100, "epoch/val_loss": test_loss / (b_idx + 1), "epoch": epoch})
+
     return test_loss / (b_idx + 1), correct / total * 100
 
 
 for epoch in range(args.epochs):
-
-    adjust_learning_rate(epoch, args, optimizer)
     
-    '''
-    other tricks
-    
-    if epoch > args.warm:
-       train_scheduler.step(epoch)
-    '''
-    
-    train_loss, train_accuracy = train(net, epoch)
-    test_loss, test_accuracy = test(net)
+    train_loss, train_accuracy = train(model, epoch)
+    test_loss, test_accuracy = test(model)
     
     print("-------------------------------------------------------------------------")
     
-    #wandb.log({"epoch/val_acc": test_accuracy, "epoch/val_loss": test_loss, "epoch/train_acc": train_accuracy, "epoch/trn_loss": train_loss, "epoch": epoch})
-
 wandb.finish()
     
